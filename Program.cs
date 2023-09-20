65b417813e7286819f4ed4f925d3de12cf4de918
Shohboz Xurramov <shohboztuit@gmail.com>
2023-09-20 07:56:05 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    public delegate void Great (bool isSingIn);
+
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;// console yozuvlariga rang berish
+        Console.WriteLine("Sariq rangda chiqadi console yozuvlar");
+        Console.WriteLine();
+        Thread.Sleep(3000);// 3 sekund console qotib turadi
+        Console.ForegroundColor= ConsoleColor .Green;//yangi rang urnatish
+        Console.WriteLine("Yashil rang urnatildi");
+        Console.WriteLine();
+       // Console.ResetColor();// consoleni dastlabgi holatiga qaytarish white and black
+
+        Console.BackgroundColor = ConsoleColor.Red;// console orqa foniga rang berish
+        Console.WriteLine("hello world , this is  a red color");
+
+    }
+
+
+    
+}
